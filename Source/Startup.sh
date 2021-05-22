9650340feb09aae3eabdb8bc23267a3c72e6fa49
Pixelboris <45537347+Pixelbo@users.noreply.github.com>
2021-05-22 13:44:07 +0200

    Update Startup.sh

--- Source/Startup.sh
+++ Source/Startup.sh
@@ -1,2 +1,2 @@
-/home/Minitel/Minitel_code/init.py ttyUSB0       # Minitel initialization
-/home/Minitel/Minitel_code/tty.py ttyUSB0 Minitel &   # Minitel terminal converter
+/home/Minitel/Minitel-raspi/Source/init.py ttyUSB0       # Minitel initialization
+/home/Minitel/Minitel-raspi/Source/tty.py ttyUSB0 Minitel &   # Minitel terminal converter
