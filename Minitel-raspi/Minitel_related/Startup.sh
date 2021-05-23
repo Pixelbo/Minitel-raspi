f03cfa88a4801754158360fe32d0d9f18ec4ecc5
Pixelboris <45537347+Pixelbo@users.noreply.github.com>
2021-05-23 19:25:00 +0200

    Delete Minitel-raspi directory

--- Minitel-raspi/Minitel_related/Startup.sh
+++
@@ -1,2 +0,0 @@
-/home/Minitel/Minitel-raspi/Minitel_related/init.py ttyUSB0       # Minitel initialization
-/home/Minitel/Minitel-raspi/Minitel_related/tty.py ttyUSB0 Minitel &   # Minitel terminal converter
--- Minitel-raspi/Project/setup.sh
+++
@@ -1,8 +0,0 @@
-cd "/home/Minitel/Minitel-raspi/Project/"
-git clone "https://github.com/Pixelbo/PyLMS"
-cd PyLMS
-python3 setup.py install
-cd ..
-rm -R PyLMS
-
-pip3 install whiptail
