ca83efa3f1f22f7c699ad5c8ed0fba4dc6886340
Pixelboris <45537347+Pixelbo@users.noreply.github.com>
2021-05-23 16:33:19 +0200

    Add files via upload

---
+++ Minitel_related/Startup.sh
@@ -0,0 +1,2 @@
+/home/Minitel/Minitel-raspi/Minitel_related/init.py ttyUSB0       # Minitel initialization
+/home/Minitel/Minitel-raspi/Minitel_related/tty.py ttyUSB0 Minitel &   # Minitel terminal converter
