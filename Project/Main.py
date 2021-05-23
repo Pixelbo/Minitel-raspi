826162e9ffb585ff6f388cacc791c16c398017ee
Pixelboris <45537347+Pixelbo@users.noreply.github.com>
2021-05-23 17:13:25 +0200

    Add files via upload

--- Project/Main.py
+++ Project/Main.py
@@ -38,7 +38,9 @@ class LMS():
         if selction == self.choix_menuLMS[6]: self.title_add()
         if selction == self.choix_menuLMS[7]: self.album_add()
         if selction == self.choix_menuLMS[8]: self.artist_lp()
-        if selction == self.choix_menuLMS[-1]: Main.menu()
+        if selction == self.choix_menuLMS[-1]: Main()
+
+
 
     def toggle(self):
         self.player.toggle()
@@ -53,14 +55,14 @@ class LMS():
         self.menu()
 
     def previous(self):
-        self.player.previous()
+        self.player.prev()
         self.menu()
 
     def lookpl(self):
-        playlist_not_treated = Player.playlist_get_info()
+        playlist_not_treated = self.player.playlist_get_info()
         playlist_treated = []
 
-        for i in range(len(playlist)):
+        for i in range(len(playlist_not_treated)):
             playlist_treated.append(
                 "%s from %s by %s" % (playlist_not_treated[i]['title'], playlist_not_treated[i]['album'], playlist_not_treated[i]['artist'])
                 )
@@ -69,17 +71,20 @@ class LMS():
             result = self.whip.showlist("radiolist",
                                         "Voici la playist actuelle, il y a %i titres, cocher des cases ne sert a rien" % (self.player.playlist_track_count(),)
                                         ,playlist_treated , "")
-            self.menu()
+
         except IndexError:
             self.whip.alert("Il y a rien dans la playlist, dommage...")
-            self.menu()
+
         except:
-            self.menu()
+            pass
+
+        self.menu()
 
     def clearpl(self):
         result = self.whip.confirm("Voulez vous vraiment faire ca?")
         if result: self.player.playlist_clear()
         self.menu()
+
 
     def title_add(self):
         result = self.whip.prompt("Mettez le titre").decode("UTF-8")
@@ -90,40 +95,41 @@ class LMS():
             song_list_treated.append("%s from %s by %s" % (song_list[i]['title'], song_list[i]['album'], song_list[i]['artist']))
 
         try:
-            result = self.whip.menu("radiolist", "Resultats de recherche" ,playlist2 , "").decode("UTF-8")
+            result = self.whip.menu("Resultats de recherche" ,song_list_treated ).decode("UTF-8")
             song = self.server.search(result, mode="songs")[1][0]['url']
             appendd = self.whip.confirm("Voulez-vous ajouter ce morceau a la playlist (oui) ou voulez-vous le jouer maintenant (non) ?")
             if appendd: self.player.playlist_add(song)
             else: self.player.playlist_play(song)
 
-            self.menu()
         except IndexError:
             whip.alert("Mauvais recherche, dommage...")
-            self.menu()
         except:
-            self.menu()
+            pass
+
+        self.menu()
 
     def album_add(self):
         result = self.whip.prompt("Mettez le titre de l'album").decode("UTF-8")
-        song_list = self.server.search(result, mode="albums")[1]
-        song_list_treated = []
+        album_list = self.server.search(result, mode="albums")[1]
+        album_list_treated = []
 
-        for i in range(len(song_list)):
-            song_list_treated.append(" %s by %s" % (song_list[i]['album'], song_list[i]['artist']))
+        for i in range(len(album_list)):
+            album_list_treated.append(" %s by %s" % (album_list[i]['album'], album_list[i]['artist']))
 
         try:
-            result = self.whip.menu("radiolist", "Resultats de recherche" ,playlist2 , "").decode("UTF-8")
-            song = self.server.search(result, mode="albums")[1][0]['url']
-            appendd = self.whip.confirm("Voulez-vous ajouter cet album a la playlist (oui) ou voulez-vous le jouer maintenant (non) ?")
-            if appendd: self.player.playlist_addalbum(song)
-            else: self.player.playlist_addalbum(song)
+            result = self.whip.menu("Resultats de recherche" ,album_list_treated).decode("UTF-8")
+            print()
+            album_ar = album_list[album_list_treated.index(result)]['artist']
+            album = album_list[album_list_treated.index(result)]['album']
+            print(album_ar, album)
+            self.player.playlist_addalbum(None, album_ar, album)
 
-            self.menu()
         except IndexError:
-            whip.alert("Mauvais recherche, dommage...")
-            self.menu()
+            self.whip.alert("Mauvais recherche, dommage...")
         except:
-            self.menu()
+            pass
+
+        self.menu()
 
     def artist_lp(self):
         result = self.whip.prompt("Mettez l'artist").decode("UTF-8")
@@ -133,13 +139,13 @@ class LMS():
             artists_treated.append("%s" % (artists[i]['artist']))
 
         try:
-            result = elf.whip.showlist("radiolist", "Resultats de recherche" ,artists_treated , "")
-            self.menu()
+            result = self.whip.showlist("radiolist", "Resultats de recherche" ,artists_treated , "")
         except IndexError:
             self.whip.alert("Mauvais recherche, dommage...")
-            self.menu()
         except:
-            self.menu()
+            pass
+
+        self.menu()
 
 
 
@@ -149,8 +155,10 @@ class Main():
 
         self.choix_menuMain = ("Music","other...", "Quitter")
 
+        self.menu()
+
     def menu(self):
-        selction = self.whip.menu("", choix_menuP).decode("UTF-8")
+        selection = self.whip.menu("", self.choix_menuMain).decode("UTF-8")
 
         if selection == self.choix_menuMain[0]: self.start_LMS()
         if selection == self.choix_menuMain[-1]: exit()
@@ -159,8 +167,6 @@ class Main():
         LMS(self.whip)
 
         self.menu()
-
-
 
 
 
