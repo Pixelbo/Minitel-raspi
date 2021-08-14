3952d1f6d516650bb472a529031cb7ffc7a66f39
Pixelbo <pixelbo21@gmail.com>
2021-08-14 16:52:44 +0200

    Request done, now menu, I guess

--- Project/Food.py
+++ Project/Food.py
@@ -1,5 +1,7 @@
 import requests
 import urllib.parse
+import json
+#import qrcode
 
 import text_utils
 
@@ -11,9 +13,9 @@ class Recipe:
         self.ID = app_id
         self.KEY = app_key
 
-        self.request("chicken", "t")
+        self.request("chicken soup", '{"diet": "low-fat"}')
 
-    # Extras list presentation : {"diet": "low-fat"}
+    # Extras list presentation in json: {"diet": "low-fat"}
     def request(self, query, extra):
         Base_url = "https://api.edamam.com/api/recipes/v2?type=public" \
                    "&q={_query}" \
@@ -23,3 +25,21 @@ class Recipe:
                    "&field=label&field=source&field=url&field=shareAs&field=yield&field=dietLabels&field=healthLabels&field=cautions&field=ingredientLines&field=ingredients&field=calories&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=totalNutrients"
 
         query = urllib.parse.quote(query)
+        extra_dict = json.loads(extra)
+
+        extra_str = ""
+        for extra in extra_dict:
+            extra_str += "&" + extra + "=" + extra_dict[extra]
+
+        URL = Base_url.format(_query=query, id=self.ID, key=self.KEY, extra=extra_str)
+
+        request = requests.get(URL)
+
+        if request.status_code != 200:
+            self.whip("Erreur WEB!, Erreur: {}".format(request.status_code))
+
+        request_JSON = json.loads(request.json())
+
+        return request_JSON
+
+    
--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ rm -r PyMinitel
 
 pip3 install pyserial
 pip3 install requests
+pip3 install qrcode
 echo 'export LANG=fr_FR.iso88591'>> /home/Minitel/.bashrc
 echo 'cd /home/Minitel/Minitel-raspi/Project/'>> /home/Minitel/.bashrc #comment this line if u don't want the project at startup
 echo './Main.py' >> /home/Minitel/.bashrc
